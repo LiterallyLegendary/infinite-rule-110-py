21b55c1e0cbe53e9f6875e45a388a52e0b46592e
LiterallyLegendary <72235715+LiterallyLegendary@users.noreply.github.com>
2025-10-29 16:09:41 -0700

    Added xpos attribute, changed input/output type of infinitestep
    The infinitestep function now takes a Rule110Position as an argument and modifies it according to Rule 110, with no output.
    The xpos attribute keeps track of the relative place of the rule110 position's non-repeating middle string of bits as it changes throughout the simulation.

--- infiniterule110.py
+++ infiniterule110.py
@@ -2,25 +2,27 @@ class Rule110Position:
     bits: bin
     llength: int #how many leftward bits repeat infinitely to the left
     rlength: int #how many rightward bits repeat infinitely to the right
+    xpos: int
 
-def infinitestep(bits: bin, llength: int, rlength:int) -> bin:
+def infinitestep(pos1: Rule110Position) -> None:
     #initialize output
-    length:int = bits.bit_length() - 1
-    if rlength <1 or llength <1:
+    length:int = pos1.bits.bit_length() - 1
+    if pos1.rlength <1 or pos1.llength <1:
         raise ValueError("don't set the length of the looping strings to less than 1.")
     #apply Rule 110 to the position
-    newbits: bin = finitestep(((((((bits>>length-llength) % 4) - 1)*(1+2*(llength==1)) <<
-                                     length) + bits) << 2) +
-                              ((bits >> (rlength - 2)) % 4  if rlength>1 else bits%2*3)) >> 1
+    newbits: bin = finitestep(((((((pos1.bits>>length-pos1.llength) % 4) - 1)*(1+2*(pos1.llength==1)) <<
+                                     length) + pos1.bits) << 2) +
+                              ((pos1.bits >> (pos1.rlength - 2)) % 4  if pos1.rlength>1 else pos1.bits%2*3)) >> 1
 
     #we need to simplify the position if bits are found in the middle string that repeat the leftmost or rightmost strings
-    cutl:int = ~(newbits>>(length - llength) ^ newbits>>length) & 2+(length>llength+1) 
-    cutr:int = ~(newbits>>rlength ^ newbits) & 1+2*(length>rlength+1)
+    cutl:int = ~(newbits>>(length - pos1.llength) ^ newbits>>length) & 2+(length>pos1.llength+1) 
+    cutr:int = ~(newbits>>pos1.rlength ^ newbits) & 1+2*(length>pos1.rlength+1)
     #the logic after the & is to make sure we're not overcutting when llength/rlength is close to length
     leading1: int = length+2- cutl//2*(1+cutl%2) - cutr%2 * (1 - cutr//2)
 
-    #cutting the new position and returning it (this program does not do it to the fullest extent possible)
-    return (newbits >> (cutr%2 * (1 + cutr//2))) % (1<<leading1) + (1<<leading1)
+    #simplifying the new position (this program does not simplify to the fullest extent possible)
+    pos1.bits = (newbits >> (cutr%2 * (1 + cutr//2))) % (1<<leading1) + (1<<leading1)
+    pos1.xpos += cutl//2*(1+cutl%2)-1
 
 def finitestep(x: bin) -> bin:
     return (~x>>1)&x|x^(x<<1)
@@ -30,14 +32,15 @@ def main() -> None:
     pos.bits = int('1'+input("Please enter a string of binary digits to simulate"),2)
     pos.llength = int(input("How many digits on the left infinitely repeat?"))
     pos.rlength = int(input("How many digits on the right infinitely repeat?"))
+    pos.xpos = 0
     j: int = int(input("How many steps would you like to simulate this for?"))
     for i in range(j):
-        pos.bits = infinitestep(pos.bits, pos.llength, pos.rlength)
+        infinitestep(pos)
         print(bin(pos.bits)[3:])
+    print(f'Relative xpos of leftmost non-repeating bit: {pos.xpos}')
     input("Simulation finished, press enter to continue")
 
 if __name__ == "__main__":
     main()
 
 #maybe i improve this with (~x)>>1&x|x^(x<<1)
-
